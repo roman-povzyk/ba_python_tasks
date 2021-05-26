a2081756ad0c570b02e0701020f9057227e326b3
romanpovzyk <roman.povzyk@gmail.com>
2021-05-26 12:42:27 +0300

    add &

--- ba_python_tasks_05/task_2.py
+++ ba_python_tasks_05/task_2.py
@@ -8,7 +8,7 @@ while i < 10:
     list_2.append(random.randint(1, 10))
     i += 1
 
-set_3 = set(list_1).intersection(set(list_2))
+set_3 = set(list_1) & set(list_2)
 list_3 = list(set_3)
 
 print(list_3)
