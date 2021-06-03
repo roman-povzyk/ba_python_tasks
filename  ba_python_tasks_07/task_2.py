b4b70ed505593e2a59b80a3955786ee9340aefb9
romanpovzyk <roman.povzyk@gmail.com>
2021-06-03 19:18:28 +0300

    tasks #8

---  ba_python_tasks_07/task_2.py
+++  ba_python_tasks_07/task_2.py
@@ -1,6 +1,10 @@
+d = {}
+
+
 def make_country(country, capital):
-    d = {country: capital}
+    d.update({country: capital})
     return d
 
 
 print(make_country('Ukraine', 'Kyiv'))
+print(make_country('India', 'New Dehli'))
