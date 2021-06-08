d57b934f78195547c39a3e4fe9a09d43cb8fcf22
romanpovzyk <roman.povzyk@gmail.com>
2021-06-08 11:29:05 +0300

    tasks #10

---
+++ ba_python_tasks_10/task_1.py
@@ -0,0 +1,19 @@
+# Make a class called Person. Make the __init__() method
+# take firstname, lastname, and age as parameters and
+# add them as attributes. Make another method called talk()
+# which makes prints a greeting from the person containing,
+# for example like this:
+# “Hello, my name is Carl Johnson and I’m 26 years old”.
+
+class Person:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def talk(self):
+        return f'Hello, my name is {self.first_name} {self.last_name} and I\'m {self.age} years old'
+
+
+man = Person('Carl', 'Johnson', 26)
+print(man.talk())
---
+++ ba_python_tasks_10/task_2.py
@@ -0,0 +1,17 @@
+# Create a class Dog with class attribute `age_factor`
+# equals to 7. Make __init__() which takes values for
+# a dog’s age. Then create a method `human_age`
+# which returns the dog’s age in human equivalent.
+
+class Dog:
+    age_factor = 7
+
+    def __init__(self, age):
+        self.age = age
+
+    def human_age(self):
+        return self.age * Dog.age_factor
+
+
+my_dog = Dog(3)
+print(my_dog.human_age())
