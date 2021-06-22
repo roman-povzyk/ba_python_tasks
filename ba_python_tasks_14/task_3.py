589673a3906d466718810512313b3698755821d9
romanpovzyk <roman.povzyk@gmail.com>
2021-06-22 14:15:20 +0300

    tasks #14

---
+++ ba_python_tasks_14/task_3.py
@@ -0,0 +1,34 @@
+# Write a decorator `arg_rules` that validates arguments passed to the function.
+# A decorator should take 3 arguments:
+# max_length: 15
+# type_: str
+# contains: [] - list of symbols that an argument should contain
+# If some of the rules' checks returns False, the function should
+# return False and print the reason it failed;
+# otherwise, return the result.
+
+def arg_rules(type_: type, max_length: int, contains: list):
+    def wrapper(func):
+        def validate(name):
+            """Переписуємо поведінку нашої функції"""
+            if type(name) == type_ and len(name) < 16:
+                for element in contains:
+                    if element in name:
+                        pass
+                    else:
+                        return False
+                return func(name)
+            else:
+                return False
+        return validate
+    return wrapper
+
+
+@arg_rules(type_=str, max_length=15, contains=['05', '@'])
+def create_slogan(name: str) -> str:
+    return f'{name} drinks pepsi in his brand new BMW!'
+
+
+print(create_slogan('johndoe05@gmail.com'))
+print(create_slogan('S@SH05'))
+
