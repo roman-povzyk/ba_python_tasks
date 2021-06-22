2d389a538b488adab04526ce8437fbc34bff6524
romanpovzyk <roman.povzyk@gmail.com>
2021-06-22 13:46:30 +0300

    tasks #14

---
+++ ba_python_tasks_14/task_2.py
@@ -0,0 +1,21 @@
+# Write a decorator that takes a list of stop
+# words and replaces them with *inside the decorated function
+def stop_words(words: list):
+    def wrapper(func):
+        def string(name):
+            """Переписуємо поведінку нашої функції"""
+            our_str = str(func(name))
+            for word in words:
+                if word.lower() in our_str.lower():
+                    our_str = our_str.replace(word, '*')
+            return our_str
+        return string
+    return wrapper
+
+
+@stop_words(['pepsi', 'BMW'])
+def create_slogan(name: str) -> str:
+    return f'{name} drinks pepsi in his brand new BMW!'
+
+
+print(create_slogan('Roman'))
