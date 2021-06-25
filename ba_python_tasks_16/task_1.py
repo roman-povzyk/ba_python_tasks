7e0ce0be41209aef0545e8ada5f7c15de06c1dbd
romanpovzyk <roman.povzyk@gmail.com>
2021-06-25 11:04:08 +0300

    tasks #16

---
+++ ba_python_tasks_16/task_1.py
@@ -0,0 +1,16 @@
+# Create your own implementation of a built-in function enumerate,
+# named 'with_index', which takes two parameters: 'iterable' and
+# 'start', default is 0. Tips: see the documentation for the enumerate function.
+
+import itertools
+
+
+def with_index(wi_iterable, wi_start=0):
+    """My own implementation of a built-in function enumerate"""
+    my_list = list(zip(itertools.count(start=wi_start), wi_iterable))
+    return my_list
+
+
+fruits = ('apple', 'banana', 'cherry')
+with_index_fruits = with_index(fruits, 1)
+print(with_index_fruits)
