86b4f64fd1513231bb16e583ff84273986189bcf
romanpovzyk <roman.povzyk@gmail.com>
2021-06-30 10:30:21 +0300

    tasks #18

---
+++ ba_pythin_tasks_18/task_18_1/task_18_1_0.py
@@ -0,0 +1,24 @@
+import random
+
+
+# створюємо функцію для вгадування числа
+def guess_number():
+    comp_number = random.randint(1, 100)
+    human_number = 0
+
+    while comp_number != human_number:
+        try:
+            human_number = int(input('Введіть число від 1 до 100: \n'))
+            if human_number < comp_number:
+                print('Треба більше!')
+            elif human_number > comp_number:
+                print('Треба менше!')
+            else:
+                print('Вгадали!')
+        except ValueError:
+            print('Потрібно ввести число!')
+
+
+# забороняємо використовувати функцію в інших файлах без виклику
+if __name__ == '__main__':
+    guess_number()
---
+++ ba_pythin_tasks_18/task_18_1/task_18_1_1.py
@@ -0,0 +1,5 @@
+from task_18_1_0 import guess_number
+
+guess_number()
+
+print('Вітаємо з виграшем!')
